dc359e979c6d0f4471a812ff52ab1221c95f9bd2
المحاسب أحمد مبروك <ahmed.mabrouk@chessmaster.com>
2025-09-21 09:27:48 -0400

    Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/chess-master/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
